0c52217278303cbda09e5bfe3f057f236cb96fed
Kevin Galligan <kgalligan@gmail.com>
2022-10-03 21:37:57 -0400

    Cocoapods github release

--- kmmbridge/src/main/kotlin/ProjectExtensions.kt
+++ kmmbridge/src/main/kotlin/ProjectExtensions.kt
@@ -1,10 +1,13 @@
 package co.touchlab.faktory
 
 import org.gradle.api.Project
+import org.gradle.api.plugins.ExtensionAware
 import org.gradle.api.plugins.ExtraPropertiesExtension
+import org.gradle.kotlin.dsl.findByType
 import org.gradle.kotlin.dsl.getByType
 import org.gradle.kotlin.dsl.provideDelegate
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.plugin.cocoapods.CocoapodsExtension
 import java.io.File
 import kotlin.reflect.KProperty
 
@@ -13,6 +16,9 @@ internal val Project.kmmBridgeExtension get() = extensions.getByType<KmmBridgeEx
 
 internal val Project.urlFile get() = file("$buildDir/faktory/url")
 
+// Cocoapods is an extension of KMP extension so you can't just do project.extensions.getByType<CocoapodsExtension>()
+internal val KotlinMultiplatformExtension.cocoapods get() = (this as ExtensionAware).extensions.findByType<CocoapodsExtension>()
+    ?: error("You must apply the org.jetbrains.kotlin.native.cocoapods plugin to use cocoapods() configuration")
 
 internal fun Project.zipFilePath(): File {
     val tempDir = file("$buildDir/faktory/zip")
