b8187f7549725b7b9f3525d52cb606ee075212e3
Michael Friend <michaelf7777@gmail.com>
2022-10-13 12:50:05 -0400

    get framework name from package instead of using project name in spm dep manager

--- kmmbridge/src/main/kotlin/KmmBridgeExtension.kt
+++ kmmbridge/src/main/kotlin/KmmBridgeExtension.kt
@@ -96,9 +96,8 @@ interface KmmBridgeExtension {
 
     fun Project.spm(
         spmDirectory: String? = null,
-        packageName: String = project.name,
     ) {
-        val dependencyManager = SpmDependencyManager(spmDirectory, packageName)
+        val dependencyManager = SpmDependencyManager(spmDirectory)
         dependencyManagers.set(dependencyManagers.getOrElse(emptyList()) + dependencyManager)
     }
 
