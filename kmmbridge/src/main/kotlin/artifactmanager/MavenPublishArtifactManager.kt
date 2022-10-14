f51036f9f98f8711191aa03904721484928c1843
Kevin Galligan <kgalligan@gmail.com>
2022-10-13 21:53:01 -0400

    Target publication specifically

--- kmmbridge/src/main/kotlin/artifactmanager/MavenPublishArtifactManager.kt
+++ kmmbridge/src/main/kotlin/artifactmanager/MavenPublishArtifactManager.kt
@@ -1,22 +1,35 @@
 package co.touchlab.faktory.artifactmanager
 
 import co.touchlab.faktory.publishingExtension
+import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.api.Task
+import org.gradle.api.artifacts.Configuration
+import org.gradle.api.artifacts.repositories.ArtifactRepository
 import org.gradle.api.artifacts.repositories.MavenArtifactRepository
+import org.gradle.api.attributes.Bundling
+import org.gradle.api.attributes.Category
+import org.gradle.api.attributes.LibraryElements
+import org.gradle.api.attributes.Usage
+import org.gradle.api.attributes.java.TargetJvmVersion
 import org.gradle.api.component.SoftwareComponentFactory
 import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.kotlin.dsl.creating
 import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.getValue
+import org.gradle.kotlin.dsl.named
 import java.io.File
+
+private const val FRAMEWORK_PUBLICATION_NAME = "KMMBridgeFramework"
 
 class MavenPublishArtifactManager(
     val project: Project,
-    private val publication: String?,
-    private val repository: String?
+    private val publicationName: String?,
+    private val repositoryName: String?
 ) : ArtifactManager {
 
-    val gradlePublishingTask: Task?
+    private val gradlePublishingTask: Task?
         get() = publishingTaskName()?.let { project.tasks.findByName(it) }
 
     private val group: String = project.group.toString().replace(".", "/")
@@ -28,16 +41,23 @@ class MavenPublishArtifactManager(
         uploadTask: Task,
         softwareComponentFactory: SoftwareComponentFactory
     ) {
+        with(softwareComponentFactory.adhoc("kmmbridge")) {
+            project.components.add(this)
+            addVariantsFromConfiguration(project.createOutgoingConfiguration()) { mapToMavenScope("runtime") }
+        }
+
+        project.publishingExtension.publications.create(FRAMEWORK_PUBLICATION_NAME, MavenPublication::class.java) {
+            from(project.components.getByName("kmmbridge"))
+            this.version = version
+            artifact(project.tasks.getByName("zipXCFramework")) {
+                classifier = "kmmbridge"
+                extension = "zip"
+            }
+        }
+
         val dependsTask = gradlePublishingTask
         // If we're not in CI, this can be null
         if(dependsTask != null) {
-            project.publishingExtension.publications.create("SharedFramework", MavenPublication::class.java) {
-                this.version = version
-                artifact(project.tasks.getByName("zipXCFramework")) {
-                    classifier = "kmmbridge"
-                    extension = "zip"
-                }
-            }
             uploadTask.dependsOn(dependsTask)
         }
     }
@@ -50,33 +70,51 @@ class MavenPublishArtifactManager(
      * maven's well known conventions.
      */
     override fun deployArtifact(project: Project, zipFilePath: File, version: String): String {
-        val publishing = project.extensions.getByType<PublishingExtension>()
+        val publishingExtension = project.extensions.getByType<PublishingExtension>()
 
-        val mavenArtifactRepository = (repository?.let {
-            publishing.repositories.findByName(it)
-        } ?: publishing.repositories.first()) as MavenArtifactRepository
+        val mavenArtifactRepository = findArtifactRepository(publishingExtension)!! // Previous code assumed not-null, so just hard not-nulling here, unless we want to deal with nulls
 
         return artifactPath(mavenArtifactRepository.url.toString(), version)
     }
 
     private fun publishingTaskName(): String? {
-        val publishing = project.extensions.getByType<PublishingExtension>()
+        val publishingExtension = project.extensions.getByType<PublishingExtension>()
 
-        val publication = publication?.let {
-            publishing.publications.findByName(it)
-        } ?: publishing.publications.firstOrNull()
-        val publicationName = publication?.name?.capitalize()
+        // Either the user has supplied a correct name, or we use the default. If neither is found, fail.
+        val publicationNameCap = publishingExtension.publications.getByName(publicationName ?: FRAMEWORK_PUBLICATION_NAME).name.capitalize()
 
-        val mavenArtifactRepository = (repository?.let {
-            publishing.repositories.findByName(it)
-        } ?: publishing.repositories.firstOrNull()) as MavenArtifactRepository?
+        val mavenArtifactRepository = findArtifactRepository(publishingExtension)
+
         val repositoryName = mavenArtifactRepository?.name?.capitalize()
 
-        return if (publicationName == null || repositoryName == null) {
+        return if (repositoryName == null) {
             null
         } else {
-            "publish${publicationName}PublicationTo${repositoryName}Repository"
+            "publish${publicationNameCap}PublicationTo${repositoryName}Repository"
         }
+    }
+
+    private fun findArtifactRepository(publishingExtension: PublishingExtension): MavenArtifactRepository? {
+        val mavenArtifactRepository = (repositoryName?.let {
+            publishingExtension.repositories.findByName(it) as MavenArtifactRepository
+        } ?: publishingExtension.repositories.filterIsInstance<MavenArtifactRepository>().firstOrNull())
+        return mavenArtifactRepository
+    }
+
+    private fun Project.createOutgoingConfiguration(): Configuration {
+        val configuration by configurations.creating {
+            isCanBeConsumed = true
+            isCanBeResolved = false
+            attributes {
+                attribute(Category.CATEGORY_ATTRIBUTE, objects.named(Category.LIBRARY))
+                attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage.NATIVE_LINK))
+                attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.EMBEDDED))
+                attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, JavaVersion.current().majorVersion.toInt())
+                attribute(LibraryElements.LIBRARY_ELEMENTS_ATTRIBUTE, objects.named("shared-xcframework"))
+            }
+        }
+
+        return configuration
     }
 
     private fun artifactPath(url: String, version: String) =
