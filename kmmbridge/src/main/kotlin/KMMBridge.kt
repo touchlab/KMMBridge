dc42510362deb75beb9fb59de5f51e15b1c80edb
Kevin Galligan <kgalligan@gmail.com>
2022-10-15 16:51:53 -0400

    Maven publish

--- kmmbridge/src/main/kotlin/KMMBridge.kt
+++ kmmbridge/src/main/kotlin/KMMBridge.kt
@@ -144,21 +144,32 @@ class KMMBridgePlugin : Plugin<Project> {
     }
 }
 
+/**
+ * Helper function to support GitHub Packages publishing. Use with https://github.com/touchlab/KMMBridgeGithubWorkflow
+ * or pass in a valid GitHub token with GITHUB_PUBLISH_TOKEN. Defaults user to "cirunner", which can be overridden with
+ * GITHUB_PUBLISH_USER.
+ *
+ * Generally, just add the following in the Gradle build file.
+ *
+ * addGithubPackagesRepository()
+ */
 @Suppress("unused")
-fun PublishingExtension.addGithubPackagesRepository(project: Project){
-    try {
-        val githubPublishUser = project.githubPublishUser ?: "cirunner"
-        val githubPublishToken = project.githubPublishToken
-        val githubRepo = project.githubRepo
-        repositories.maven {
-            name = "GitHubPackages"
-            url = URI.create("https://maven.pkg.github.com/$githubRepo")
-            credentials {
-                username = githubPublishUser//project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                password = githubPublishToken//project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+fun Project.addGithubPackagesRepository() {
+    publishingExtension.also {
+        try {
+            val githubPublishUser = project.githubPublishUser ?: "cirunner"
+            val githubPublishToken = project.githubPublishToken
+            val githubRepo = project.githubRepo
+            repositories.maven {
+                name = "GitHubPackages"
+                url = URI.create("https://maven.pkg.github.com/$githubRepo")
+                credentials {
+                    username = githubPublishUser//project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                    password = githubPublishToken//project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+                }
             }
+        } catch (e: Exception) {
+            // Ignore if not in CI
         }
-    } catch (e: Exception) {
-        // Ignore if not in CI
     }
 }
