99c0e157b8bdd5f65cb7a336cbef0c071f50f828
Julia Jakubcova <j.jakubcova@touchlab.co>
2022-12-15 09:41:10 +0100

    Revert "Fix version managers not respecting if writing tags is enabled."

--- kmmbridge/src/main/kotlin/versionmanager/GithubEnterpriseVersionManager.kt
+++ kmmbridge/src/main/kotlin/versionmanager/GithubEnterpriseVersionManager.kt
@@ -1,6 +1,5 @@
 package co.touchlab.faktory.versionmanager
 
-import co.touchlab.faktory.alwaysWriteGitTags
 import co.touchlab.faktory.githubRepo
 import co.touchlab.faktory.internal.GithubEnterpriseCalls
 import co.touchlab.faktory.internal.procRunFailLog
@@ -9,10 +8,6 @@ import org.gradle.api.Project
 object GithubEnterpriseReleaseVersionManager : GitTagBasedVersionManager() {
     override fun recordVersion(project: Project, versionString: String) {
         val commitId = project.procRunFailLog("git", "rev-parse", "HEAD").first()
-        if (project.alwaysWriteGitTags){
-            GithubEnterpriseCalls.createRelease(project, project.githubRepo, versionString, commitId)
-        } else {
-            GithubEnterpriseCalls.createRelease(project, project.githubRepo, null, commitId)
-        }
+        GithubEnterpriseCalls.createRelease(project, project.githubRepo, versionString, commitId)
     }
 }
