3d5bf72d86e736a231ecd1f8965e86f930ef1a88
Kevin Galligan <kgalligan@gmail.com>
2023-03-07 10:43:42 -0500

    Revert link

--- website/src/theme/DocPaginator/index.js
+++ website/src/theme/DocPaginator/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import DocPaginator from '@theme-original/DocPaginator';
 import NewsletterDoc from '@site/src/components/NewsletterDoc';
-import TouchlabPro from '@site/src/components/TouchlabPro';
 
 export default function DocPaginatorWrapper(props) {
   return (
       <>
           <DocPaginator {...props} />
-          <div className="mt-8"><TouchlabPro/></div>
+          <div className="mt-8"><NewsletterDoc/></div>
       </>
   );
 }
