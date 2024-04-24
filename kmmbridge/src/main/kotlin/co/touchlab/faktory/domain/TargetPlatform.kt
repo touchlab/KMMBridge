10f1ab1cf2676c73a650a40bb5688cd7fa2c029a
slam <slamhan1987@gmail.com>
2024-04-24 19:47:13 +0800

    fix(#244) - Need to add mac, tv, and watch options  - support custom swiftToolsVersion  - support custom platform targets

---
+++ kmmbridge/src/main/kotlin/co/touchlab/faktory/domain/TargetPlatform.kt
@@ -0,0 +1,18 @@
+package co.touchlab.faktory.domain
+
+
+data class TargetPlatform(
+    val name: PlatformName,
+    val version: PlatformVersion,
+)
+
+enum class PlatformName(val value: String) {
+    MacOS("macOS"),
+    IOS("iOS"),
+    TvOS("tvOS"),
+    WatchOS("watchOS"),
+}
+
+data class PlatformVersion(
+    val version: String = "",
+)
