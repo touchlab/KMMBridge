aaf5623003ee8db38030a7f26d78ce848eb9d4e3
Gustavo Fao Valvassori <faogustavo@gmail.com>
2024-07-30 16:25:54 -0300

    allowing multiple packages in the same package.swift file

--- kmmbridge/src/test/kotlin/co/touchlab/faktory/dependencyManager/PackageFileUpdateTest.kt
+++ kmmbridge/src/test/kotlin/co/touchlab/faktory/dependencyManager/PackageFileUpdateTest.kt
@@ -74,6 +74,7 @@ class PackageFileUpdateTest {
         val newFile = getModifiedPackageFileText(
             oldFile,
             "TestPackage",
+            false,
             "https://www.example.com/",
             "fedcba9876543210"
         )
@@ -105,6 +106,95 @@ class PackageFileUpdateTest {
         val newFile = getModifiedPackageFileText(
             oldFile,
             "TestPackage",
+            false,
+            "https://www.example.com/",
+            "fedcba9876543210"
+        )
+        assertEquals(expectedNewFile, newFile)
+    }
+
+    @Test
+    fun withMultipleModules() {
+        val oldFile = """
+            // swift-tools-version:5.3
+            import PackageDescription
+
+            // BEGIN KMMBRIDGE VARIABLES BLOCK FOR 'TestPackage' (do not edit)
+            let remoteTestPackageUrl = "https://www.example.com/"
+            let remoteTestPackageChecksum = "01234567890abcdef"
+            let testPackagePackageName = "TestPackage"
+            // END KMMBRIDGE BLOCK FOR 'TestPackage'
+
+            // BEGIN KMMBRIDGE VARIABLES BLOCK FOR 'TestPackage2' (do not edit)
+            let remoteTestPackageUrl = "https://www.example.com/"
+            let remoteTestPackageChecksum = "01234567890abcdeg"
+            let testPackagePackageName = "TestPackage2"
+            // END KMMBRIDGE BLOCK FOR 'TestPackage2'
+
+            let package = Package(
+                name: packageName,
+                platforms: [
+                    .iOS(.v13)
+                ],
+                products: [
+                    .library(
+                        name: packageName,
+                        targets: [packageName]
+                    ),
+                ],
+                targets: [
+                    .binaryTarget(
+                        name: packageName,
+                        url: remoteKotlinUrl,
+                        checksum: remoteKotlinChecksum
+                    )
+                    ,
+                ]
+            )
+        """.trimIndent()
+
+        val expectedNewFile = """
+            // swift-tools-version:5.3
+            import PackageDescription
+
+            // BEGIN KMMBRIDGE VARIABLES BLOCK FOR 'TestPackage' (do not edit)
+            let remoteTestPackageUrl = "https://www.example.com/"
+            let remoteTestPackageChecksum = "fedcba9876543210"
+            let testPackagePackageName = "TestPackage"
+            // END KMMBRIDGE BLOCK FOR 'TestPackage'
+
+            // BEGIN KMMBRIDGE VARIABLES BLOCK FOR 'TestPackage2' (do not edit)
+            let remoteTestPackageUrl = "https://www.example.com/"
+            let remoteTestPackageChecksum = "01234567890abcdeg"
+            let testPackagePackageName = "TestPackage2"
+            // END KMMBRIDGE BLOCK FOR 'TestPackage2'
+
+            let package = Package(
+                name: packageName,
+                platforms: [
+                    .iOS(.v13)
+                ],
+                products: [
+                    .library(
+                        name: packageName,
+                        targets: [packageName]
+                    ),
+                ],
+                targets: [
+                    .binaryTarget(
+                        name: packageName,
+                        url: remoteKotlinUrl,
+                        checksum: remoteKotlinChecksum
+                    )
+                    ,
+                ]
+            )
+        """.trimIndent()
+
+        val newFile = getModifiedPackageFileText(
+            oldFile,
+            "TestPackage",
+            true,
             "https://www.example.com/",
             "fedcba9876543210"
         )
