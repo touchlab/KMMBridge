7bd3322b538e897012a7cc490d433c9bdb674dba
Kevin Galligan <kgalligan@gmail.com>
2024-12-08 10:37:37 -0500

    Externally create release

--- kmmbridge-github/src/main/kotlin/co/touchlab/kmmbridge/github/GithubReleaseArtifactManager.kt
+++ kmmbridge-github/src/main/kotlin/co/touchlab/kmmbridge/github/GithubReleaseArtifactManager.kt
@@ -3,6 +3,8 @@ package co.touchlab.kmmbridge.github
 import co.touchlab.kmmbridge.KmmBridgeExtension
 import co.touchlab.kmmbridge.artifactmanager.ArtifactManager
 import co.touchlab.kmmbridge.github.internal.GithubCalls
+import co.touchlab.kmmbridge.github.internal.githubArtifactIdentifierName
+import co.touchlab.kmmbridge.github.internal.githubArtifactReleaseId
 import co.touchlab.kmmbridge.github.internal.githubPublishToken
 import co.touchlab.kmmbridge.github.internal.githubRepo
 import org.gradle.api.GradleException
@@ -12,9 +14,14 @@ import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskProvider
 import org.gradle.kotlin.dsl.getByType
 import java.io.File
+import kotlin.properties.Delegates
 
-internal class GithubReleaseArtifactManager(
-    private val repository: String?, private val releaseString: String?, private val useExistingRelease: Boolean
+open class GithubReleaseArtifactManager(
+    private val repository: String?,
+    private val releaseString: String?,
+    @Deprecated("Releases should be created externally. This parameter controls the flow for releases created " +
+            "by this class, which will eventually be unsupported.")
+    private val useExistingRelease: Boolean
 ) : ArtifactManager {
 
     @get:Input
@@ -26,42 +33,53 @@ internal class GithubReleaseArtifactManager(
     @get:Input
     lateinit var frameworkName: String
 
+    @get:Input
+    lateinit var artifactIdentifierName: String
+
+    @get:Input
+    var artifactReleaseId by Delegates.notNull<Int>()
+
     // TODO: This value is stored in the config cache. It is encrypted, but this still feels insecure. Review alternatives.
     // https://docs.gradle.org/current/userguide/configuration_cache.html#config_cache:secrets
     lateinit var githubPublishToken: String
 
     override fun configure(
-        project: Project,
-        version: String,
-        uploadTask: TaskProvider<Task>,
-        kmmPublishTask: TaskProvider<Task>
+        project: Project, version: String, uploadTask: TaskProvider<Task>, kmmPublishTask: TaskProvider<Task>
     ) {
         this.releaseVersion = releaseString ?: project.version.toString()
         this.repoName = this.repository ?: project.githubRepo
         this.githubPublishToken = project.githubPublishToken
         this.frameworkName = project.kmmBridgeExtension.frameworkName.get()
+        artifactReleaseId = project.githubArtifactReleaseId?.toInt() ?: 0
+        this.artifactIdentifierName = project.githubArtifactIdentifierName ?: ""
     }
 
     override fun deployArtifact(task: Task, zipFilePath: File, version: String): String {
-        val existingReleaseId = GithubCalls.findReleaseId(
-            githubPublishToken, repoName, releaseVersion
-        )
+        val uploadReleaseId = if (artifactReleaseId == 0) {
+            val existingReleaseId = GithubCalls.findReleaseId(
+                githubPublishToken, repoName, releaseVersion
+            )
 
-        task.logger.info("existingReleaseId: $existingReleaseId")
+            task.logger.info("existingReleaseId: $existingReleaseId")
 
-        if (existingReleaseId != null && !useExistingRelease) {
-            throw GradleException("Release for '$releaseVersion' exists. Set 'useExistingRelease = true' to update existing releases.")
+            if (existingReleaseId != null && !useExistingRelease) {
+                throw GradleException("Release for '$releaseVersion' exists. Set 'useExistingRelease = true' to update existing releases.")
+            }
+
+            val idReply: Int = existingReleaseId ?: GithubCalls.createRelease(
+                githubPublishToken, repoName, releaseVersion, null
+            )
+
+            task.logger.info("GitHub Release created with id: $idReply")
+
+            idReply
+        } else {
+            artifactReleaseId
         }
-
-        val idReply: Int = existingReleaseId ?: GithubCalls.createRelease(
-            githubPublishToken, repoName, releaseVersion, null
-        )
-
-        task.logger.info("GitHub Release created with id: $idReply")
 
         val fileName = artifactName(version, useExistingRelease)
 
-        val uploadUrl = GithubCalls.uploadZipFile(githubPublishToken, zipFilePath, repoName, idReply, fileName)
+        val uploadUrl = GithubCalls.uploadZipFile(githubPublishToken, zipFilePath, repoName, uploadReleaseId, fileName)
         return "${uploadUrl}.zip"
     }
 
@@ -69,8 +87,14 @@ internal class GithubReleaseArtifactManager(
         return if (useExistingRelease) {
             "$frameworkName-${versionString}-${(System.currentTimeMillis() / 1000)}.xcframework.zip"
         } else {
-            "$frameworkName.xcframework.zip"
+            uploadZipFileName(versionString)
         }
+    }
+
+    open fun uploadZipFileName(versionString: String) = if (artifactIdentifierName.isNotEmpty()) {
+        "$frameworkName-${artifactIdentifierName}.xcframework.zip"
+    } else {
+        "$frameworkName.xcframework.zip"
     }
 }
 
